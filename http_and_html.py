abf9aa7d373a537d148a31f86bbeeb12d0c97344
Your Name <you@example.com>
2019-04-14 20:53:06 +0800

    bug fix (in line 10):TypeError

--- http_and_html.py
+++ http_and_html.py
@@ -7,7 +7,7 @@ def parse_http_response(response):
     header="\n".join("\n".join(res[1:]).split("\n\n")[0].splitlines()[1:])
     header_dict={}
     for x in header:
-        header_dict[x.split(0)]=x.split(1)
+        header_dict[x.split()[0]]=x.split()[1]
     response_dict=copy.deepcopy(header_dict)
     version=第一行.split()[0]
     status=第一行.split()[1]

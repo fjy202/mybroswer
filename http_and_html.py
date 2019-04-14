9444cbf633223644cb9a8fe265f52b7922226c54
Your Name <you@example.com>
2019-04-14 21:01:36 +0800

    fix bugs

--- http_and_html.py
+++ http_and_html.py
@@ -4,10 +4,10 @@ import copy
 def parse_http_response(response):
     res=response.split("\n")
     第一行=res[0]
-    header="\n".join("\n".join(res[1:]).split("\n\n")[0].splitlines()[1:])
+    header="\n".join(res[1:]).split("\n\n")[0]
     header_dict={}
-    for x in header:
-        header_dict[x.split()[0]]=x.split()[1]
+    for x in header.splitlines():
+        header_dict[x.split(":")[0]]=x.split(":")[1]
     response_dict=copy.deepcopy(header_dict)
     version=第一行.split()[0]
     status=第一行.split()[1]
